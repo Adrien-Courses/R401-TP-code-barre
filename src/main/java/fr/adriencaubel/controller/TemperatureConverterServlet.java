ef9cd4f89eb1f009fd4755b598c18772bd9a605d
adrien1212 <batman.caubel@laposte.net>
2025-02-09 11:07:50 +0100

    initial

---
+++ src/main/java/fr/adriencaubel/controller/TemperatureConverterServlet.java
@@ -0,0 +1,5 @@
+package fr.adriencaubel.controller;
+
+public class TemperatureConverterServlet {
+
+}
---
+++ src/main/java/fr/adriencaubel/model/TemperatureConverter.java
@@ -0,0 +1,7 @@
+package fr.adriencaubel.model;
+
+public class TemperatureConverter {
+	public static double convertCelsiusToFahrenheit(double celsius) {
+		return (celsius * 9 / 5) + 32;
+	}
+}
